8dec99b4cb04c0a6950f3d8760dd1a62f1a377e0
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2023-11-24 19:20:21 +0500

    React

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
+
+    const [blogs, setBlogs] = useState([
+        { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 1 },
+        { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 2 },
+        { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 3 }
+    ])
+
+    return (
         <div className="home">
-            <h2>HomePage</h2>
+            <BlogList />
         </div>
-     );
+    );
 }
- 
+
 export default Home;
