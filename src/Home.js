4aef9c68107805010858eac9f41de40f189d664b
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2023-11-25 20:06:04 +0500

    React

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,21 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
     const [blogs, setBlogs] = useState([
-        { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 1 },
+        { title: "My new website", body: "Lorem Ipsum...", author: "Asma", id: 1 },
         { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 2 },
-        { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 3 }
+        { title: "My new website", body: "Lorem Ipsum...", author: "Asma", id: 3 }
     ])
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
 
     return (
         <div className="home">
-            <BlogList />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Asma')} title="Asma's Blog" /> */}
         </div>
     );
 }
