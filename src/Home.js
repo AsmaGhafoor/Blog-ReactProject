2f7fa41c6a174885046d5c1df39d74247be515a5
AsmaGhafoor <asmaghafoorofficial@gmail.com>
2023-12-01 23:24:17 +0500

    React

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "My new website", body: "Lorem Ipsum...", author: "Asma", id: 1 },
-        { title: "My new website", body: "Lorem Ipsum...", author: "mario", id: 2 },
-        { title: "My new website", body: "Lorem Ipsum...", author: "Asma", id: 3 }
-    ])
+    const [blogs, setBlogs] = useState([null])
+
+    // const [name, setName] = useState('mario');
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(() => {
+        // console.log('Use Effect Run!');
+        // console.log(blogs);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data=>{
+                setBlogs(data);// set nhin kia tha apney useState mein null jaa rahi thi values blogs={blogs} idhr
+            })
+    }, []);// what does empty array mean? btayen whatsap pr
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Asma')} title="Asma's Blog" /> */}
+            {/* <button onClick={() => setName('AsmaG')}>Change Name</button> */}
+            {/* <p>{name}</p> */}
         </div>
     );
 }
